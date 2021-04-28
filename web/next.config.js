a23a4c16c02b8f11acb8ea25da6dfa308494cc6a
Aura-n <reikage46497@gmail.com>
2021-04-28 13:47:06 +0900

    Add antd custom theme

--- web/next.config.js
+++ web/next.config.js
@@ -1,12 +1,19 @@
-// next.config.js
-const withLess = require("next-with-less");
+const withLess = require("next-with-less")
+const lessToJS = require('less-vars-to-js')
+const fs = require('fs')
+const path = require('path')
+
+// Where your antd-custom.less file lives
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './styles/antd-custom.less'), 'utf8')
+)
 
 module.exports = withLess({
   future: {
     webpack5: true,
   },
 
-  // lessOptions: {
-  //   /* ... */
-  // },
+  lessOptions: {
+    modifyVars: themeVariables,
+  },
 });
