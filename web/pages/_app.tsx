de472a7209b659621a4cf16bf352223f3958b4c9
Aura-n <reikage46497@gmail.com>
2021-04-30 15:36:01 +0900

    Add nprogress

--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,30 @@
 import { AppProps } from 'next/app'
 import '../styles/globals.scss'
 import 'antd/dist/antd.less'
+import nprogress from 'nprogress'
+import { useRouter } from 'next/router'
+import '../static/scss/nprogress.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const router = useRouter()
+  // nprogress 設定
+  nprogress.configure()
+
+  // ページ遷移時
+  router.events?.on('routeChangeStart', () => {
+    nprogress.start()
+    console.log("Move")
+  })
+  // ページ遷移完了
+  router.events?.on('routeChangeComplete', () => {
+    nprogress.done()
+  })
+  // ページ遷移時エラー
+  router.events?.on('routeChangeError', () => {
+    nprogress.done()
+  })
+
+  return (
+    <Component {...pageProps} />
+  )
 }
