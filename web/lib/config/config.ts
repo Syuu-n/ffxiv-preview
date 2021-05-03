1046b650be5eef7642620598d6d1c868fb97fdfc
Aura-n <reikage46497@gmail.com>
2021-05-03 13:23:00 +0900

    Update config.ts

--- web/lib/config/config.ts
+++ web/lib/config/config.ts
@@ -7,4 +7,3 @@ export const SITE_DESCRIPTION = "サイトのデスクリプション"
 export const SSR_BASE_URL = "http://localhost:3001/api"
 
 export const LODESTONE_ITEM_BASE_URL ="https://jp.finalfantasyxiv.com/lodestone/playguide/db/item"
-// export const LODESTONE_TOOLTIP_
